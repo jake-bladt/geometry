7c4e9c79429ac8d3d8deefc90bdbb6e4cd08f53f
Jake Bladt <jake@jakebladt.com>
2019-03-04 15:59:39 -0500

    first-pass code

---
+++ Geometry/CartesianPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class CartesianPoint
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public double DistanceFrom(CartesianPoint that)
+        {
+            return Math.Sqrt(Math.Pow((this.x - that.x) + (this.y - that.y), 2));
+        }
+    }
+}
